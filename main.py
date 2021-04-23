5626391e5dea5075909571043e44fe2ef8836129
ZioGio22 <giovanni.vagnoli@gmail.com>
2021-04-23 23:05:40 +0200

    Modified the path of ff++ programs an added graphs

--- main.py
+++ main.py
@@ -9,19 +9,50 @@ subprocess.Popen("mkdir -p Flows", shell=True)
 subprocess.call("Echo. > Results.txt", shell = True)
 
 #mesh generation
-mesh_gen = "FreeFem++ mesh_gen.edp"
+mesh_gen = "FreeFem++ FF++solvers/mesh_gen.edp"
 subprocess.call(mesh_gen, shell = True)
 
 #selecting Reynolds number between 45 and 49 (instability appears at Re = 47)
 Re_test = np.linspace(45.,49.,10)
 
 #initial guess (Stokes's flow)
-ig = "FreeFem++ initial_guess.edp"
+ig = "FreeFem++ FF++solvers/initial_guess.edp"
 subprocess.call(ig, shell = True)
 
 #loop over Reynolds numbers
 for i in Re_test:
-    bf = "FreeFem++ baseflow.edp -Re "+ str(i)
+    bf = "FreeFem++ FF++solvers/baseflow.edp -Re "+ str(i)
     subprocess.call(bf, shell = True)
-    ei = "mpirun -np 2 FreeFem++-mpi eig_eval.edp -Re "+ str(i)
+    ei = "mpirun -np 1 FreeFem++-mpi FF++solvers/eig_eval.edp -Re "+ str(i)
     subprocess.call(ei, shell = True)
+
+# plotting the Results
+#read the Eigenvalues
+results = open("Results.txt", 'r')
+lines = results.readlines()
+
+la_im = []
+la_re = []
+
+for i in lines:
+    la_re.append(float(i.split(' ')[1]))
+    la_im.append(float(i.split(' ')[2]))
+
+results.close
+
+#building the figure
+fig, (re, im) = plt.subplots(2,1)
+
+fig.suptitle("Eigenvalues - Reynolds")
+re.plot(Re_test, la_re)
+
+re.set(ylabel = 'Real(lambda)')
+re.grid()
+
+im.plot(Re_test, la_im)
+
+im.set(xlabel = 'Re', ylabel = 'Imag(lambda)')
+im.grid()
+
+#save the figure
+fig.savefig("Results.png")
