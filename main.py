89a5d889ad0f19b86ac428c12bbe9e869b641f2e
ZioGio22 <giovanni.vagnoli@gmail.com>
2021-04-23 19:21:28 +0200

    Added the cycle on Re

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 # evaluate critical Reynolds number for the primary instability of a circular
 #cylinder
 import subprocess
-subprocess.call("mkdir esa", shell = True, cwd = "prova")
+import numpy as np
+import matplotlib.pyplot as plt
+
+subprocess.call("Echo. > Results.txt", shell = True)
+#selecting Reynolds number between 45 and 49 (instability appears at Re = 47)
+Re_test = np.linspace(45.,49.,10)
+
+for i in Re_test:
+    string = "FreeFem++ input_test.edp -Re "+ str(i)
+    print(string)
+    subprocess.call(string, shell = True)
